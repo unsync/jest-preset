ac96810355ac570d59d4278a465367fdd848b039
unsync <unsync@example.com>
2024-01-27 18:28:46 +0100

    init

--- jest-preset.cjs
+++ jest-preset.cjs
@@ -1,17 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  // doesn't work with shared config
+  // preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
+    "^.+\\.tsx?$": [
+      "ts-jest",
       {
         useESM: true,
       },
     ],
   },
-};
+}
