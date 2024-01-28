1def27daf16eb749d2a9c5941c03de21bfc67390
unsync <unsync@example.com>
2024-01-28 17:12:35 +0100

    update

--- jest-preset.cjs
+++ jest-preset.cjs
@@ -2,15 +2,15 @@
 module.exports = {
   // doesn't work with shared config
   // preset: "ts-jest/presets/default-esm",
-  extensionsToTreatAsEsm: [".ts"],
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    "^.+\\.tsx?$": [
-      "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
       {
         useESM: true,
       },
--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,27 +1,13 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
 
-const scriptName = 'test'
-const scriptCommand = 'npm run build && node --experimental-vm-modules node_modules/jest/bin/jest.js'
+const packageJsonPath = `${require.main.paths[0].split('node_modules')[0]}package.json`
+if (fs.existsSync(packageJsonPath)) {
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+  console.info('postinstall: add jest-preset script')
+  packageJson.scripts.test = 'npm run build && node --experimental-vm-modules node_modules/jest/bin/jest.js'
+  packageJson.jest = {
+    preset: '@unsync/jest-preset',
+  }
 
-console.info(`postinstall: starting`)
-
-const packageJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json'
-if(!fs.existsSync(packageJsonPath)) {
-    console.info(`postinstall: ${packageJsonPath} do not exist`)
-    return
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
 }
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
-
-if(packageJson.name === '@unsync/jest-preset') {
-    console.info(`postinstall: do not run on ${packageJson.name}`)
-    return
-}
-
-console.info('postinstall: add jest-preset script')
-packageJson.scripts[scriptName] = scriptCommand
-packageJson.jest = {
-    "preset": "@unsync/jest-preset"
-}
-
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
